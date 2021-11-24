65a3697127793773410cee37df95b9c02e51f67b
Zachary Droge <zdroge@macbook-pro-3.myfiosgateway.com>
2021-11-24 13:20:41 -0500

    Added comments and cleaned up the spacing

--- Topping.js
+++ Topping.js
@@ -1,10 +1,15 @@
-// Functional Component
+// Functional Component where we import react library and write a named function to be used as a component elsewhere in react app
 import React from "react"
 
 const Topping = (props) => {
 
+    // We are taking the object of props (like attributes on an html tag) and they are being placed inside an object ---> props object
+    //When we reference props we then can access a key value pair, like props.type.
     return <p>{props.type}</p>
 
 }   
+
+// We need to export this component so we can use it else where in out app. 
+// This is a front end module thing (webpack) and not explicitly a react thing. we take advantage of this to modulize our code in react
 
 export default Topping
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+// react library is being used to give us access to write react code, but also things like "Component" to give us extra functionality in out class base components
 import React, {Component} from 'react';
+// this library react-dom is being used to "connect" out react app to the entry index.html
 import ReactDOM from 'react-dom';
+//we want to use this component from ./Topping file so we import it here. Used in render's return
 import Topping from './Topping'
 
-// we went to out DB to get toppings - async activity that came back as
-
+// toppingsFromDb represents if we went to out DB to get toppings - async activity that came back as....toppingsFromDb
 let toppingsFromDb = ["Cheese", "Peperoni", "Funghi", "Pineapple", "peppers"]
-
 
 class WholeApp extends Component{
   constructor(props){
@@ -13,33 +14,33 @@ class WholeApp extends Component{
     this.state = {
       selectedTopping: "Cheese"
     }
-
   }
   render(){
-    // this.props
+    // this.props is how we would get props into any classbased component.
     return (
       <div>
-        {/* <Topping type="Cheese" otherStuff="thisString" />
+        {/* 
+        This is using a single components to hard code new values within out prop objects, specifically "type"
+
+        <Topping type="Cheese" otherStuff="thisString" />
         <Topping type="Peperoni" />
-        <Topping type="Funghi" /> */}
+        <Topping type="Funghi" /> 
         
-
-        {/* If we got out data back form the Database */}
-
+        */}
+        
+        {/* If we got out data back form the Database we can loop through to create a list of components with specific data for each*/}
         {
-        
-        toppingsFromDb.map((topping, idx) => {
-          return <Topping key={topping + idx} type={topping} />
-        })
-
+          toppingsFromDb.map((topping, idx) => {
+            return <Topping key={topping + idx} type={topping} />
+          })
         }
       </div>
-      
     )
   }
 }
 
 
 
-
+//WholeApp is being referenced here becuase it will be connected to the html node with id "app" in inde.html
+// We have access to this becuase we are in the same file.
 ReactDOM.render(<WholeApp />, document.getElementById('app'));
